7dcb2a671b1d274999593937ce67b6d8655246c9
priya <priyaasinghh345@gmail.com>
2023-09-11 18:28:35 +0530

    basics

--- App.js
+++ App.js
@@ -1,37 +1,23 @@
 
-//import from node_modules
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-
-//second parameter of React.createElement() is attribute and third is children
-//both children and attribute comes in props if we see in console
-
-
-/*if we want to have a nested html like 
-<div id="parent">
-    <div id="child">
-        <h1>nested </h1>
-        <h2>sibling </h2>
-    </div> 
-</div>*/
-
-const element = React.createElement(
-    "div", {id: "parent"}, React.createElement(
-        "div", {id: "child"}, [React.createElement(
-            "h1", {}, "nested "
-            ), React.createElement(
-                "h2", {}, "sibling"
-                )
-            ]
-        )
+//this is functional component
+const Heading = () => {
+    return (
+        <h1>This is heading from component</h1>
     )
+}
 
+let num = 1000
+//functional component without return 
+const HeadingWithoutReturn = () => (
+    <h1>heading without return</h1>
+)
 
+//jsxHeading is react element
+const jsxHeading = <h1>This is heading{num+100+233+num}</h1>
 
-const head = React.createElement("h1", {id: "head"}, "Hello World from React")
-
-//this will print an object which is a react element 
-console.log (head)
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(element)
+root.render(jsxHeading)
+
